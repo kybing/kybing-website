162d906f16d4700ea9c2da186b3d4ca3783cf412
Uday Sravan Kumar Kamineni <uday@kybing.com>
2020-07-09 23:43:17 -0500

    Changing home page url in package.json didn't fix static content invalid url on gh pages. Looks like adding router fixed it

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to KYBING
+          Hello there, Welcome to KYBING 
         </p>
       </header>
     </div>
