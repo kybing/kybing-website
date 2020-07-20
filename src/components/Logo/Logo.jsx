661336e93ccd66643e5b58080bdbc59322886881
Anusha Patil <anusha@kybing.com>
2020-07-20 03:21:33 -0500

    App started working - yet to resolve eslint issue.

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -4,6 +4,6 @@ import logo from '../../assets/images/logo.png'
 
 import './Logo.css'
 
-const Logo = () => <img src={logo} className='Logo-img' alt='logo' />
+const Logo = () => <img src={logo} className="Logo-img" alt="logo" />
 
 export default Logo
